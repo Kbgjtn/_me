1e324fb2169029088047568ba7a90f5c9920ae65
Kambingjan31 <83516023+Kbgjtn@users.noreply.github.com>
2023-01-03 13:21:30 +0700

    fix(style): reading list background

--- src/components/templates/book/book.tsx
+++ src/components/templates/book/book.tsx
@@ -3,7 +3,7 @@ import projectsBackground from "../../../assets/images/projects-bg-me.png";
 
 const Book = () => {
 	return (
-		<div className="w-full h-auto bg-black flex flex-auto">
+		<div className="w-full h-full bg-black flex flex-auto">
 			<Image
 				className="w-full h-auto bg-no-repeat"
 				src={projectsBackground}
