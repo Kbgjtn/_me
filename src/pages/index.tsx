830040f652dcd0c2b6bd47c0d9de9b7532eae3c6
Kbgjtn <kambingjan31@gmail.com>
2023-01-12 00:49:20 +0700

    feat(seo): optimize seo with meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,18 @@ export default function Home() {
 		<>
 			<Head>
 				<title>DaffaRqlme</title>
-				<meta name="description" content="Hi, here i'm daffa" />
+				<meta name="description" content="Hi, here i'm daffa!" />
 				<meta
 					name="viewport"
 					content="width=device-width, initial-scale=1.0"
 				/>
+				<meta property="og:title" content="Hi, here i'm daffa!" />
+				<meta
+					property="og:description"
+					content="About - Hi, here i'm daffa!"
+				/>
+				<meta property="og:url" content="https://coocobolo.com/" />
+				<meta property="og:type" content="website"></meta>
 				<meta httpEquiv="X-UA-Compatible" content="ie=edge" />
 				<link rel="icon" href="/tortuga.ico" />
 			</Head>
