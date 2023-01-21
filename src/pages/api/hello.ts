7b89ffe49a12768c336889cb0e969425afeb01f5
Kbgjtn <kambingjan31@gmail.com>
2023-01-21 18:31:07 +0700

    fix(api): redirect response from api/hello

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req);
+  if (process.env.NODE_ENV === "production")
+    return res.redirect("https://www.coocobolo.com");
+  if (process.env.NODE_ENV === "development")
+    return res.redirect("http://localhost:3000");
 }
