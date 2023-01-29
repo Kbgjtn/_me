9d6fbf99a46738874875fd10968eea264c1fea1d
Kambingjan31 <83516023+Kbgjtn@users.noreply.github.com>
2023-01-30 05:01:54 +0700

    feat(scroll): add new scroll in nav to reading list
    Signed-off-by: Kambingjan31 <83516023+Kbgjtn@users.noreply.github.com>

--- src/components/modules/navbar/navbar.tsx
+++ src/components/modules/navbar/navbar.tsx
@@ -4,81 +4,93 @@ import { useState } from "react";
 import Sosmed from "../modal/sosmed";
 
 const NavBar = () => {
-	const [isHoverHMU, setIsHoverHMU] = useState(false);
+  const [isHoverHMU, setIsHoverHMU] = useState(false);
 
-	const handleHoverHMU = () => {
-		setIsHoverHMU(!isHoverHMU as boolean);
-	};
+  const handleHoverHMU = () => {
+    setIsHoverHMU(!isHoverHMU as boolean);
+  };
 
-	return (
-		<motion.div
-			className="w-full h-auto flex absolute items-center justify-center top-0 z-10 py-4 px-11"
-			initial="hidden"
-			animate="visible"
-			exit="exit">
-			<motion.dl
-				className="
+  const handleClickScroll = () => {
+    const element = document.getElementById("reading-list");
+    if (element) {
+      // 👇 Will scroll smoothly to the top of the next section
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <motion.div
+      className="w-full h-auto flex absolute items-center justify-center top-0 z-10 py-4 px-11"
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <motion.dl
+        className="
 					inline-flex font-semibold text-white font-cascode text-xl gap-20
 					xl:text-2xl"
-				variants={item}>
-				<motion.dt variants={item}>
-					<Link
-						href="/"
-						referrerPolicy={"no-referrer"}
-						draggable={false}>
-						me
-					</Link>
-				</motion.dt>
-				<motion.dt variants={item}>
-					<Link
-						href="https://github.com/Kbgjtn"
-						target={"_blank"}
-						referrerPolicy={"no-referrer"}
-						draggable={false}>
-						repos
-					</Link>
-				</motion.dt>
-				<motion.dt
-					variants={item}
-					onMouseEnter={() => handleHoverHMU()}
-					onMouseLeave={() => handleHoverHMU()}>
-					hmu
-				</motion.dt>
-			</motion.dl>
-			<Sosmed isHover={isHoverHMU} />
-		</motion.div>
-	);
+        variants={item}
+      >
+        <motion.dt variants={item}>
+          <Link href="" referrerPolicy={"no-referrer"} draggable={false}>
+            me
+          </Link>
+        </motion.dt>
+        <motion.dt variants={item}>
+          <Link
+            href="https://github.com/Kbgjtn"
+            target={"_blank"}
+            referrerPolicy={"no-referrer"}
+            draggable={false}
+          >
+            repos
+          </Link>
+        </motion.dt>
+        <motion.dt>
+          <p
+            className="cursor-pointer"
+            onMouseEnter={() => handleHoverHMU()}
+            onMouseLeave={() => handleHoverHMU()}
+            onClick={() => handleClickScroll()}
+          >
+            books
+          </p>
+        </motion.dt>
+      </motion.dl>
+      <Sosmed isHover={isHoverHMU} />
+    </motion.div>
+  );
 };
 
 export default NavBar;
 
 const item = {
-	hidden: { y: -20, opacity: 0 },
-	visible: {
-		y: 0,
-		opacity: 1,
-		transition: {
-			type: "spring",
-			stiffness: 260,
-			damping: 15,
-		},
-	},
-	exit: { y: 20, opacity: 0 },
+  hidden: { y: -20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      stiffness: 260,
+      damping: 15,
+    },
+  },
+  exit: { y: 20, opacity: 0 },
 };
 
 const transtionListItem = {
-	hidden: {
-		x: "-30px",
-		opacity: 0,
-	},
-	visible: {
-		x: 0,
-		opacity: 1,
-		transition: {
-			delay: 0.6,
-			stiffness: 250,
-			damping: 25,
-		},
-	},
-	exit: { y: 20, opacity: 0 },
+  hidden: {
+    x: "-30px",
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.6,
+      stiffness: 250,
+      damping: 25,
+    },
+  },
+  exit: { y: 20, opacity: 0 },
 };
--- src/components/templates/book/book.tsx
+++ src/components/templates/book/book.tsx
@@ -3,39 +3,44 @@ import projectsBackground from "../../../assets/images/projects-bg-me.png";
 import BookSlider from "./book-slider";
 
 const Book = () => {
-	return (
-		<div className="w-full h-full flex justify-center items-center bg-black">
-			<Image
-				className="inline-flex object-cover noselect z-0 min-w-full min-h-full flex-col overflow-hidden"
-				src={projectsBackground}
-				alt=""
-				draggable={"false"}
-				referrerPolicy={"strict-origin-when-cross-origin"}
-				width={1920}
-				height={1080}
-				quality={100}
-				onContextMenu={(e) => e.preventDefault()}
-			/>
+  return (
+    <div
+      id="reading-list"
+      className="w-full h-full flex justify-center items-center bg-black"
+    >
+      <Image
+        className="inline-flex object-cover noselect z-0 min-w-full min-h-full flex-col overflow-hidden"
+        src={projectsBackground}
+        alt=""
+        draggable={"false"}
+        referrerPolicy={"strict-origin-when-cross-origin"}
+        width={1920}
+        height={1080}
+        quality={100}
+        onContextMenu={(e) => e.preventDefault()}
+      />
 
-			<div
-				className="flex justify-self-center self-center absolute w-full h-full m-auto items-center justify-center flex-col"
-				aria-describedby="tp-container">
-				<div className="inline-flex relative self-center justify-center items-center">
-					<h3 className="tp-trigger text-6xl font-semibold text-center sm:text-7xl md:text-8xl lg:text-8xl xl:text-9xl 2xl:text-10xl">
-						Reading List
-					</h3>
-					<div
-						id="tp-container"
-						className="font-cascode font-medium text-xs"
-						role="tooltip">
-						Here is a list of all the books that I have read, and
-						hopefully it can add new references for all my friends
-					</div>
-				</div>
-				<BookSlider />
-			</div>
-		</div>
-	);
+      <div
+        className="flex justify-self-center self-center absolute w-full h-full m-auto items-center justify-center flex-col"
+        aria-describedby="tp-container"
+      >
+        <div className="inline-flex relative self-center justify-center items-center">
+          <h3 className="tp-trigger text-6xl font-semibold text-center sm:text-7xl md:text-8xl lg:text-8xl xl:text-9xl 2xl:text-10xl">
+            Reading List
+          </h3>
+          <div
+            id="tp-container"
+            className="font-cascode font-medium text-xs"
+            role="tooltip"
+          >
+            Here is a list of all the books that I have read, and hopefully it
+            can add new references for all my friends
+          </div>
+        </div>
+        <BookSlider />
+      </div>
+    </div>
+  );
 };
 
 export default Book;
