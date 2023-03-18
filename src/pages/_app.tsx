87e20dca14a52d96c1972fcc9f644017f908ed79
Kbgjtn <kambingjan31@gmail.com>
2023-03-19 05:53:06 +0700

    feat: using font local

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,28 +4,44 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import "../hooks/css/global.css";
 import * as gtag from "../lib/gtag";
+import localFont from "next/font/local";
 
+const assetsFontPath = "../assets/fonts/CascadiaCode/woff2/static";
+const cascadiaCode = localFont({
+  src: [
+    {
+      path: "../assets/fonts/CascadiaCode/woff2/static/CascadiaCode-Regular.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/CascadiaCode/woff2/static/CascadiaMono-Light.woff2",
+      weight: "400",
+      style: "thin",
+    },
+  ],
+});
 const isProduction = process.env.NODE_ENV === "production";
 
 export default function App({ Component, pageProps }: AppProps) {
-	const router = useRouter();
+  const router = useRouter();
 
-	useEffect(() => {
-		const handleRouteChange = (url: URL) => {
-			if (isProduction) gtag.pageview(url);
-		};
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      if (isProduction) gtag.pageview(url);
+    };
 
-		router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("routeChangeComplete", handleRouteChange);
 
-		return () => {
-			router.events.off("routeChangeComplete", handleRouteChange);
-		};
-	}, [router.events]);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
-	return (
-		<>
-			<Component {...pageProps} />
-			<Analytics />
-		</>
-	);
+  return (
+    <main className={cascadiaCode.className}>
+      <Component {...pageProps} />
+      <Analytics />
+    </main>
+  );
 }
