567ca4b5b5d2c5a60a7655db5041697b6f1d3ebd
Kbgjtn <kambingjan31@gmail.com>
2023-03-19 05:52:11 +0700

    feat: add text & svg for the progress project page

--- src/components/templates/project/project.tsx
+++ src/components/templates/project/project.tsx
@@ -1,13 +1,19 @@
+import FrameMountain from "../../../assets/images/FrameMountain";
+
 const Project = () => {
-	return (
-		<div
-			className="w-full h-full flex justify-center items-center"
-			style={{
-				background: "#0a0a0a",
-			}}>
-			<div>project</div>
-		</div>
-	);
+  return (
+    <div
+      id="project-list"
+      className="w-full h-full flex bg-charcoal justify-center items-center"
+    >
+      <div className="flex relative font-medium justify-center items-center text-center text-sm">
+        <h4 className="absolute m-auto z-30">Under Development:)</h4>
+        <div className="flex w-full h-full relative">
+          <FrameMountain />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
