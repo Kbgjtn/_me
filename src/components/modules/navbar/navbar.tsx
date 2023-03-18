c4522b2fbb46f957f0eba086eb416b88022054a4
Kbgjtn <kambingjan31@gmail.com>
2023-03-19 05:48:30 +0700

    refactor: return react component using map array list of the navbar list

--- src/components/modules/navbar/navbar.tsx
+++ src/components/modules/navbar/navbar.tsx
@@ -1,15 +1,12 @@
 import { motion } from "framer-motion";
-import Link from "next/link";
+import GithubLogoSvg from "../../../assets/images/GithubLogo";
+import BookLogoSvg from "../../../assets/images/BookLogo";
+import ProjectLogoSvg from "../../../assets/images/ProjectLogo";
+import MeLogo from "../../../assets/images/MeLogo";
 import { useState } from "react";
-import Sosmed from "../modal/sosmed";
 
 const NavBar = () => {
-  const [isHoverHMU, setIsHoverHMU] = useState(false);
-
-  const handleHoverHMU = () => {
-    setIsHoverHMU(!isHoverHMU as boolean);
-  };
-
+  const [hoverLogoName, setHoverLogoName] = useState<string>("");
   const handleClickScroll = () => {
     const element = document.getElementById("reading-list");
     if (element) {
@@ -19,46 +16,49 @@ const NavBar = () => {
   };
 
   return (
-    <motion.div
-      className="w-full h-auto flex absolute items-center justify-center top-0 z-10 py-4 px-11"
+    <motion.nav
+      className="w-full h-auto bg-charcoal flex flex-col relative items-center justify-center top-0 z-1 py-4 px-11 "
       initial="hidden"
       animate="visible"
       exit="exit"
     >
       <motion.dl
-        className="
-					inline-flex font-semibold text-white font-cascode text-xl gap-20
-					xl:text-2xl"
+        className="flex relative justify-center items-center font-medium text-white font-cascode text-xl gap-[2rem] xl:text-2xl"
         variants={item}
       >
-        <motion.dt variants={item}>
-          <Link href="" referrerPolicy={"no-referrer"} draggable={false}>
-            me
-          </Link>
-        </motion.dt>
-        <motion.dt variants={item}>
-          <Link
-            href="https://github.com/Kbgjtn"
-            target={"_blank"}
-            referrerPolicy={"no-referrer"}
+        {[
+          ["me", "/", <MeLogo title={hoverLogoName} />],
+          [
+            "repository",
+            "https://github.com/Kbgjtn",
+            <GithubLogoSvg title={hoverLogoName} />,
+          ],
+          ["book", "#reading-list", <BookLogoSvg title={hoverLogoName} />],
+          [
+            "project",
+            "#project-list",
+            <ProjectLogoSvg title={hoverLogoName} />,
+          ],
+        ].map(([title, url, svg]) => (
+          <a
+            href={url as string}
+            onMouseEnter={() => setHoverLogoName(title as string)}
+            onMouseLeave={() => setHoverLogoName("")}
             draggable={false}
+            style={{ transition: "500ms" }}
+            referrerPolicy={"no-referrer"}
+            title={title as string}
           >
-            repos
-          </Link>
-        </motion.dt>
-        <motion.dt>
-          <p
-            className="cursor-pointer"
-            onMouseEnter={() => handleHoverHMU()}
-            onMouseLeave={() => handleHoverHMU()}
-            onClick={() => handleClickScroll()}
-          >
-            books
-          </p>
-        </motion.dt>
+            {svg}
+          </a>
+        ))}
       </motion.dl>
-      <Sosmed isHover={isHoverHMU} />
-    </motion.div>
+      {hoverLogoName ? (
+        <div className="text-center font-medium absolute top-20 mx-auto opacity-30">
+          {`./${hoverLogoName}`}
+        </div>
+      ) : null}
+    </motion.nav>
   );
 };
 
@@ -94,3 +94,32 @@ const transtionListItem = {
   },
   exit: { y: 20, opacity: 0 },
 };
+
+const a = `
+<motion.dl
+          className="
+					inline-flex font-semibold text-white font-cascode text-xl gap-[2rem]
+					xl:text-2xl"
+          variants={item}
+        >
+          <motion.dt
+            variants={item}
+            onMouseEnter={handleHoverLogo}
+            onMouseLeave={handleUnhoverLogo}
+          >
+            <Link
+              href={"https://github.com/Kbgjtn"}
+              target={"_blank"}
+              referrerPolicy={"no-referrer"}
+              draggable={false}
+            >
+              <GithubLogoSvg isHover={isHoverLogo} />
+            </Link>
+          </motion.dt>
+          <motion.dt>
+            <p className="cursor-pointer" onClick={() => handleClickScroll()}>
+              <BookLogoSvg isHover={isHoverLogo} />
+            </p>
+          </motion.dt>
+        </motion.dl>
+`;
