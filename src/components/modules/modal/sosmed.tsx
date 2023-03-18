c9523460d94f70e8e5d03e4609cb2508f11747c3
Kbgjtn <kambingjan31@gmail.com>
2023-03-19 05:47:41 +0700

    refactor: indentation

--- src/components/modules/modal/sosmed.tsx
+++ src/components/modules/modal/sosmed.tsx
@@ -1,11 +1,11 @@
 const Sosmed = (props: PropsSosmedArgs) => {
-	return props.isHover ? (
-		<div className="flex w-auto absolute z-50 h-auto top-15 left-14"></div>
-	) : null;
+  return props.isHover ? (
+    <div className="flex w-auto absolute z-50 h-auto top-15 left-14"></div>
+  ) : null;
 };
 
 export default Sosmed;
 
 type PropsSosmedArgs = {
-	isHover: Boolean;
+  isHover: Boolean;
 };
