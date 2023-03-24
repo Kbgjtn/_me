1e7e66b651211b10ee5cab8731b8cb96537872bc
Kbgjtn <kambingjan31@gmail.com>
2023-03-25 06:54:05 +0700

    feat: add new masonry layout grid for project list

--- src/components/templates/project/project.tsx
+++ src/components/templates/project/project.tsx
@@ -3,32 +3,30 @@ import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import BookImages from "../../../assets/images/books";
 import FrameMountain from "../../../assets/images/FrameMountain";
+import Masonry from "../../modules/Mansory/masonry";
 
 const Project = () => {
-	return (
-		<>
-			<Head>
-				<title>Daffa Requelme - Project</title>
-				<link
-					rel="canonical"
-					href="https://example.com/blog/original-post"
-					key="canonical"
-				/>
-			</Head>
-			<div
-				id="project-list"
-				className="w-full h-full flex bg-charcoal justify-center items-center">
-				<div className="flex relative font-medium justify-center items-center text-center text-sm">
-					<h4 className="absolute m-auto z-30">
-						Under Development :)
-					</h4>
-					<div className="flex w-full h-full relative">
-						<FrameMountain />
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Daffa Requelme - Project</title>
+        <link
+          rel="canonical"
+          href="https://example.com/blog/original-post"
+          key="canonical"
+        />
+      </Head>
+      <Masonry />
+      <div className="w-full h-full flex bg-charcoal justify-center items-center">
+        <div className="flex relative font-medium justify-center items-center text-center text-sm">
+          <h4 className="absolute m-auto z-30">Under Development :)</h4>
+          <div className="flex w-full h-full relative">
+            <FrameMountain />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Project;
