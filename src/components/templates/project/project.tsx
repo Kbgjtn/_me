e91bd948aacf171f521569e13c1233460d0caef5
Kbgjtn <kambingjan31@gmail.com>
2023-05-07 16:56:38 +0700

    chore: delete head in project page

--- src/components/templates/project/project.tsx
+++ src/components/templates/project/project.tsx
@@ -1,19 +1,9 @@
-import Head from "next/head";
-import FrameMountain from "../../../assets/images/FrameMountain";
 import Masonry from "../../modules/Mansory/masonry";
 import { seederProject } from "../../modules/Mansory/seederProject";
 
 const Project = () => {
   return (
     <>
-      <Head>
-        <title>Daffa Requelme - Project</title>
-        <link
-          rel="canonical"
-          href="https://example.com/blog/original-post"
-          key="canonical"
-        />
-      </Head>
       <Masonry title={"Project"} projects={seederProject} />
     </>
   );
