2281beee18e0465485faee7d5c41cca1ea5b5ee5
Kbgjtn <kambingjan31@gmail.com>
2023-05-09 22:56:12 +0700

    fix(font): changes the classname and variables of fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,47 +1,20 @@
 import { Analytics } from "@vercel/analytics/react";
+import Provider from "@/providers";
+
+import "@/styles/main.css";
+
 import type { AppProps } from "next/app";
-import localFont from "next/font/local";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import "../hooks/css/main.css";
-import * as gtag from "../lib/gtag";
+import Core from "@/layouts";
 
-const cascadiaCode = localFont({
-	src: [
-		{
-			path: "../assets/fonts/CascadiaCode/woff2/static/CascadiaCode-Regular.woff2",
-			weight: "500",
-			style: "normal",
-		},
-		{
-			path: "../assets/fonts/CascadiaCode/woff2/static/CascadiaMono-Light.woff2",
-			weight: "400",
-			style: "thin",
-		},
-	],
-});
-
-const isProduction = process.env.NODE_ENV === "production";
-
-export default function App({ Component, pageProps }: AppProps) {
-	const router = useRouter();
-
-	useEffect(() => {
-		const handleRouteChange = (url: URL) => {
-			if (isProduction) gtag.pageview(url);
-		};
-
-		router.events.on("routeChangeComplete", handleRouteChange);
-
-		return () => {
-			router.events.off("routeChangeComplete", handleRouteChange);
-		};
-	}, [router.events]);
-
-	return (
-		<main className={cascadiaCode.className}>
-			<Component {...pageProps} />
-			<Analytics />
-		</main>
-	);
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Provider>
+      <Core>
+        <Component {...pageProps} />
+        <Analytics />
+      </Core>
+    </Provider>
+  );
 }
+
+export default App;
