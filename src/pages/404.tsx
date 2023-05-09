98230c9e77286e7b10a021d77623d665e3f7faa8
Kbgjtn <kambingjan31@gmail.com>
2023-05-09 22:58:28 +0700

    chore: delete analytics

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,17 @@
-import MeThreeCanvas from "@/components/contents/tree";
 import Head from "next/head";
 
 function NotFound() {
-	return (
-		<>
-			<Head>
-				<title>Page Not Found</title>
-				<link rel="icon" href="/favicon.ico" />
-				<meta name="robots" content="noindex" />
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>Page Not Found</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="noindex" />
+      </Head>
 
-			<div className="w-screen h-screen bg-[#1a1a1a]"></div>
-		</>
-	);
+      <div className="w-screen h-screen bg-[#1a1a1a]"></div>
+    </>
+  );
 }
 
 export default NotFound;
